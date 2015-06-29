d1320612a133dabdcc35602f580a54ae0b002991
Mathieu Schroeter <schroeter@epsitec.ch>
2015-06-29 11:27:19 +0200

    Seperate the moduleName and the prefix for the logs.

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var prefix = 'GreatHall';
 
 var log = function (mode, msg) {
   busClient.events.send ('widget.text.' + mode, {
-    prefix: prefix + '/' + msg.moduleName + ': ',
+    prefix: prefix,
+    mod:    msg.moduleName,
     text:   msg.message
   });
 };
@@ -22,7 +23,8 @@ module.exports = function (xLog) {
   return {
     progress: function (topic, position, length) {
       busClient.events.send ('widget.progress', {
-        prefix:   prefix + '/' + xLog.getModuleName () + ': ',
+        prefix:   prefix,
+        mod:      xLog.getModuleName (),
         topic:    topic,
         position: position,
         length:   length
