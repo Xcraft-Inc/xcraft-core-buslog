016f1d6ec63c9febe1f26375cf0f457a12219498
Gillian Fries <fries@epsitec.ch>
2020-11-09 16:38:23 +0100

    Add id generated from xcraft-core-goblin

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ BusLog.prototype.log = function (mode, msg) {
     this._resp.command.send('overwatch.exception', {
       error: {
         ...(msg.overwatch || {err: msg.message}),
+        id: msg.id,
         mod: msg.module,
         time: msg.time,
       },
